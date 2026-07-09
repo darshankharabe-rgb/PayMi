073df066f18c435e22a47fbbe9d89c3ab7d27836
Darshan Kharabe <darshkharabe@gmail.com>
2026-07-09 11:13:11 +0000

    Added database connection and sql alchemy

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
 from app.schemas import UserCreate, UserOut, UserLogin, Token
 from app.auth import hash_password, verify_password, create_access_token, decode_access_token
-
-# temporary in-memory store, will replace with real DB
-fake_users_db = []
+from app.database import get_db
+from app.models import User
 
 
 app = FastAPI(title="PayMi Backend Service", version="1.0.0")
@@ -18,39 +21,36 @@ def health_check():
     return {"status": "ok"}
 
 @app.post("/register", response_model=UserOut)
-def register_user(user: UserCreate):
-    # Check if user already exists
-    for existing_user in fake_users_db:
-        if existing_user["email"] == user.email:
-            raise HTTPException(status_code=400, detail="Email already registered")
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        hashed_password=hash_password(user.password),
+    )
+    db.add(new_user)
 
-    # Create a new user and add to the in-memory store
-    new_user = {
-        "id" : len(fake_users_db) + 1,
-        "name" : user.name,
-        "email" : user.email,
-        "password" : hash_password(user.password)  # In a real application hashed
-    }
-    fake_users_db.append(new_user)
+    try:
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Email already registered")
+    db.refresh(new_user)
     return new_user
 
 @app.post("/login", response_model=Token)
-def login(credentials: UserLogin):
-    user = None
-    for existing_user in fake_users_db:
-        if existing_user["email"] == credentials.email:
-            user = existing_user
-            break
+def login(credentials: UserLogin, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == credentials.email).first()
     
-    if not user or not verify_password(credentials.password, user["password"]):
+    
+    if not user or not verify_password(credentials.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid email or password")
     
-    access_token = create_access_token(data={"sub": str(user["id"])})
+    access_token = create_access_token(data={"sub": str(user.id)})
     return {"access_token": access_token, "token_type": "bearer"}
 
 security = HTTPBearer()
 
-def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security), db: Session = Depends(get_db)):
     token = credentials.credentials
     try:
         payload = decode_access_token(token)
@@ -58,11 +58,11 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
         raise HTTPException(status_code=401, detail="Invalid or expired token")
     
     user_id = payload.get("sub")
-    for user in fake_users_db:
-        if str(user["id"]) == user_id:
-            return user
+    user = db.query(User).filter(User.id == int(user_id)).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
     
-    raise HTTPException(status_code=401, detail="User not found")
+    return user
 
 @app.get("/me", response_model=UserOut)
 def read_current_user(current_user: dict = Depends(get_current_user)):
