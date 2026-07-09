c294e0a026179d1b05e6794c43df952671a291ca
Darshan Kharabe <darshkharabe@gmail.com>
2026-07-09 08:40:27 +0000

    requirments

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI(title="PayMi Backend Service", version="1.0.0")
 
+@app.get("/")
+def root():
+    return {"message": "Welcome to the PayMi Backend Service!"}
+
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
