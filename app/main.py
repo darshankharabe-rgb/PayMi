ef5a72799dc61917e935c2778dd2821b3847873e
Darshan Kharabe <darshkharabe@gmail.com>
2026-07-09 09:17:10 +0000

    Added bcrypt Password hashing and Email duplicates check

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.schemas import UserCreate, UserOut
+from app.auth import hash_password
+
+# temporary in-memory store, will replace with real DB
+fake_users_db = []
+
 
 app = FastAPI(title="PayMi Backend Service", version="1.0.0")
 
@@ -9,3 +15,20 @@ def root():
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.post("/register", response_model=UserOut)
+def register_user(user: UserCreate):
+    # Check if user already exists
+    for existing_user in fake_users_db:
+        if existing_user["email"] == user.email:
+            raise HTTPException(status_code=400, detail="Email already registered")
+
+    # Create a new user and add to the in-memory store
+    new_user = {
+        "id" : len(fake_users_db) + 1,
+        "name" : user.name,
+        "email" : user.email,
+        "password" : hash_password(user.password)  # In a real application hashed
+    }
+    fake_users_db.append(new_user)
+    return new_user
